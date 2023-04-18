19052ce2fce1d8402194cfd336ac5be5ede9d447
Jatin Undhad <jatinundhad33@gmail.com>
2023-04-18 21:39:10 +0530

    initial commit

---
+++ DiamondMarket/src/java/AuthenticationServlets/RegisterForm.java
@@ -0,0 +1,17 @@
+package AuthenticationServlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterForm extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        request.getRequestDispatcher("/Authentication/Register.html").forward(request, response);
+    }
+}
